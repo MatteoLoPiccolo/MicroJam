8e52b42898ea1b5041bb2fd57e220581e24dc584
Matteo Lo Piccolo <lostsoul81x@gmail.com>
2021-03-05 03:11:05 +0100

    Added follow mechanic

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float _maxSpeed;
+    [SerializeField] float _mediumSpeed;
+    [SerializeField] float _lowSpeed;
+
+    float _maxDistance = 20f;
+    float _mediumDistance = 13f;
+    //float _lowDistance = 5f;
+    float _delay = 1.5f;
+    Transform _target;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        if (_target == null)
+            Debug.LogError("The Player is NULL.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(Chasing());
+    }
+
+    private IEnumerator Chasing()
+    {
+        yield return new WaitForSeconds(_delay);
+
+        var distance = Vector3.Distance(_target.position, transform.position);
+
+        if (distance >= _maxDistance)
+            transform.position = Vector3.MoveTowards(transform.position, _target.position, _maxSpeed * Time.deltaTime);
+
+        if (distance < _maxDistance && distance >= _mediumDistance)
+            transform.position = Vector3.MoveTowards(transform.position, _target.position, _mediumSpeed * Time.deltaTime);
+
+        else
+            transform.position = Vector3.MoveTowards(transform.position, _target.position, _lowSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float _speed = 7;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(horizontal * _speed * Time.deltaTime, 0, vertical * _speed * Time.deltaTime);
+    }
+}
